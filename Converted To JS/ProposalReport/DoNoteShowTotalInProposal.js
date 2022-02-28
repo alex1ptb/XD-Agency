c39812e4c408988e7f16a2c5f6fd9e5da82bafba
Alex <alex1ptb@gmail.com>
2022-02-27 23:26:31 -0500

    update copy and paste to work, add in noshowproposalbutton

--- Converted To JS/ProposalReport/DoNoteShowTotalInProposal.js
+++ Converted To JS/ProposalReport/DoNoteShowTotalInProposal.js
@@ -1,72 +1,71 @@
-Sub DoNotShowInTotalsProposal()
-'
-' DoNotShowInTotalsProposal Macro
-'
-
-'
-    Range("AA25:AA842").Select
-    Application.CutCopyMode = False
-    Selection.Copy
-    Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
-        :=False, Transpose:=False
-    Range("AD25:AD222").Select
-    Application.CutCopyMode = False
-    Selection.Copy
-    Range("E25").Select
-    ActiveSheet.Paste
-    Range("AD25").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD43").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD65").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD85").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD106").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD127").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD149").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD171").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD193").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD215").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD235").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("AD252").Select
-    ActiveWindow.LargeScroll Down:=-1
-    Range("AD241:AD842").Select
-    Application.CutCopyMode = False
-    Selection.Copy
-    Range("A17").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A35").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A57").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A77").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A98").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A119").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A141").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A163").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A185").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("A207").Select
-    ActiveWindow.LargeScroll Down:=1
-    Range("F241").Select
-    ActiveSheet.Paste
-    Range("D13").Select
-    Application.CutCopyMode = False
-    ActiveCell.FormulaR1C1 = "< ON"
-    Range("D11").Select
-    Selection.ClearContents
-    Range("A17").Select
-End Sub
+function DoNotShowInTotalsProposal() {
+  // '
+  // ' DoNotShowInTotalsProposal Macro
+  // '
+  // '
+  //     Range("AA25:AA842").Select
+  //     Application.CutCopyMode = False
+  //     Selection.Copy
+  //     Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+  //         :=False, Transpose:=False
+  //     Range("AD25:AD222").Select
+  //     Application.CutCopyMode = False
+  //     Selection.Copy
+  //     Range("E25").Select
+  //     ActiveSheet.Paste
+  //     Range("AD25").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD43").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD65").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD85").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD106").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD127").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD149").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD171").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD193").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD215").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD235").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("AD252").Select
+  //     ActiveWindow.LargeScroll Down:=-1
+  //     Range("AD241:AD842").Select
+  //     Application.CutCopyMode = False
+  //     Selection.Copy
+  //     Range("A17").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A35").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A57").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A77").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A98").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A119").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A141").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A163").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A185").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("A207").Select
+  //     ActiveWindow.LargeScroll Down:=1
+  //     Range("F241").Select
+  //     ActiveSheet.Paste
+  //     Range("D13").Select
+  //     Application.CutCopyMode = False
+  //     ActiveCell.FormulaR1C1 = "< ON"
+  //     Range("D11").Select
+  //     Selection.ClearContents
+  //     Range("A17").Select
+}
