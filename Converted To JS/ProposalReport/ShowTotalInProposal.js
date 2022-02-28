888628c8b263d6f9933860c74cacc278fd10a50b
Alex <alex1ptb@gmail.com>
2022-02-28 00:04:37 -0500

    fixing helpful function issues and updating showTotals

--- Converted To JS/ProposalReport/ShowTotalInProposal.js
+++ Converted To JS/ProposalReport/ShowTotalInProposal.js
@@ -2,22 +2,20 @@ function ShowInTotalsProposal() {
   //'
   //' ShowInTotalsProposal Macro
   //'
-
   //target sheet: Proposal
   ss = SpreadsheetApp.getActiveSpreadsheet();
-  sheet = ss.getSheetByName("Proposal");
-
+  let sheet = ss.getSheetByName("Proposal");
   //copy range AA25:AA222
   //paste range E25:E222
-  copyAndPaste(sheet, "AA25:AA222", "E25:E222");
+  copyAndPaste("AA25:AA222", "E25:E222");
 
   //copy range AA241:AA842
   //paste range F241:F842
-  copyAndPaste(sheet, "AA241:AA842", "F241:F842");
+  copyAndPaste("AA241:AA842", "F241:F842");
 
   //copy range AB25:AB842
   //paste range AA25:AA842
-  copyAndPaste(sheet, "AB25:AB842", "AA25:AA842");
+  copyAndPaste("AB25:AB842", "AA25:AA842");
 
   //select D13 and clear contents
   sheet.getRange("D13").clearContent();
