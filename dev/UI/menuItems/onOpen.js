de34ce089d59d3210bad526e90e43dc55472229e
Alex <alex1ptb@gmail.com>
2022-03-17 07:18:59 -0400

    add filter for categories already on deliverable

---
+++ dev/UI/menuItems/onOpen.js
@@ -0,0 +1,20 @@
+/**
+ * @OnlyCurrentDoc
+ */
+
+//constant variables for the entire spreadsheet
+const projectId = "xd-agency";
+
+////////
+//create onOpen function to create a menu ui
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu("Create Deliverable")
+    .addItem("Create Deliverable", "createNewDeliverableTab")
+    .addItem(
+      "Add Category to Current Deliverable",
+      "addCategoryToCurrentDeliverable"
+    )
+    .addToUi();
+  getXdaRates();
+}
---
+++ dev/UI/sideBar/deliverableSidebar.js
@@ -0,0 +1,9 @@
+//create sidebar for deliverable options
+//target ChooseAgent sheet
+
+function createSidebar() {
+  let spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  let sheet = spreadsheet.getSheetByName("ChooseAgent");
+
+  //create sidebar
+}
