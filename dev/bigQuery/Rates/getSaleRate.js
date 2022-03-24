55fbd83f39ae98fff50f236bb2ff3eb98f7399cd
Alex <alex1ptb@gmail.com>
2022-03-24 00:59:55 -0400

    now the getSaleRate works

--- dev/bigQuery/Rates/getSaleRate.js
+++ dev/bigQuery/Rates/getSaleRate.js
@@ -11,8 +11,6 @@
 //This gets the sale rate for the job choosen.
 function getSaleRate(e) {
   const namedRange = getNamedRange(e);
-  SpreadsheetApp.getUi().alert(namedRange);
-  return;
   // getNamedRange(e);
   const sheet = SpreadsheetApp.getActiveSheet();
   const activeRange = e.range;
@@ -31,7 +29,22 @@ function getSaleRate(e) {
     // Logger.log(`test: ${test}`);
     const tables = getXDATable();
 
-    //loop through the tables array and find the tableData that matches the jobTitle
+    //loop through the tables array and find the tableId that matches the namedRange
+    for (let i = 0; i < tables.length; i++) {
+      if (tables[i].tableId === namedRange) {
+        //return the tableData
+        const tableData = tables[i].tableData;
+        //loop through the tableData and find the job title that matches the job title from the cell that was edited
+        for (let j = 0; j < tableData.length; j++) {
+          if (tableData[j][0] === jobTitle) {
+            //return the sale rate
+            const saleRate = tableData[j][1];
+            //set the value of column 6 to the sale rate
+            sheet.getRange(row, 6).setValue(saleRate);
+          }
+        }
+      }
+    }
 
     //I am thinking that I should have this run differently in the future, using callbacks and hash tables of the job titles and their sale rates.
 
