e32b4a7b5b42d98ed7eb7495818a00241f7745a0
Alex <alex1ptb@gmail.com>
2022-04-16 23:41:58 -0400

    nothing to report

--- dev/Deliverables/makeNewDeliverablefromUserInput.js
+++ dev/Deliverables/makeNewDeliverablefromUserInput.js
@@ -1,8 +1,5 @@
 //this takes the data from the user for the title and the categories and makes a new sheet inside of the spreadsheet. The name of the spreadsheet will be the title provided by the user. And the starting categories will be pulled from the array provided by the user.
 function makeNewDeliverablefromUserInput(title, categories) {
-  console.log(`inside makeNewDeliverable`);
-  console.log(`title: ${title}`);
-  console.log(`categories: ${categories}`);
   let templateSheet = ss.getSheetByName("Deliverable_Template");
   console.log(`inside server creating sheet named: ${title}`);
   ss.insertSheet(title);
